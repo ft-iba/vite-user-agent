6474632b04b8c1403734fc77bdfc42557aad904b
daisuke iba <d.iba@futurek.com>
2025-10-14 12:41:58 +0900

    add: UA判定を表示

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,46 @@
+import './global.css'
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const isAndroidMobile = () => {
+  return /Android.+Mobile/.test(navigator.userAgent);
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const isMobile = () => {
+  return /iPhone|Android.+Mobile/.test(navigator.userAgent);
+}
+
+const isiOS = () => { // iPadは除外
+  return /iPhone|iPod/.test(navigator.userAgent);
+}
+
+const isiPadOS = () => { // iPadOS 13以降は UA が Mac
+  return (
+    navigator.userAgent.includes('iPad') ||
+    (navigator.userAgent.includes('Macintosh') && 'ontouchend' in document)
+  );
+}
+
+const functionList = { isAndroidMobile, isMobile, isiOS, isiPadOS };
+const app = document.getElementById('app')!;
+
+for (const [item, fn] of Object.entries(functionList)) {
+  const section = document.createElement('section');
+  const heading = document.createElement('h1');
+  heading.textContent = item;
+
+  const nodeCode = document.createElement('pre');
+  nodeCode.textContent = fn.toString();
+
+  const nodeResult = document.createElement('p');
+  nodeResult.textContent = `Result: ${fn()}`;
+
+  section.appendChild(heading);
+  section.appendChild(nodeCode);
+  section.appendChild(nodeResult);
+
+  app.appendChild(section);
+}
+
+const nodeUA = document.createElement('p');
+nodeUA.textContent = navigator.userAgent;
+app.appendChild(nodeUA);
